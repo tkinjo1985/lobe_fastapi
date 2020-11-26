d3572f2e040d211c6a7bd59b36dc0366cd17d23b
Kinjo.Takumi <tkinjo1@gmail.com>
2020-11-26 11:26:23 +0900

    Review of image pre-processing

--- image_utils.py
+++ image_utils.py
@@ -1,11 +1,17 @@
+
 from base64 import b64encode
 from io import BytesIO
+
 from PIL import Image
 
 
-def image2base64(image):
-    image = Image.open(image)
-    buffered = BytesIO()
-    image.save(buffered, format="JPEG")
-    img_b64 = b64encode(buffered.getvalue())
-    return img_b64.decode('utf8')
+def image2base64(image_file):
+    try:
+        image = Image.open(image_file).convert('RGB')
+    except Exception as err:
+        print(err)
+    else:
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        img_b64 = b64encode(buffered.getvalue())
+        return img_b64.decode('utf8')
